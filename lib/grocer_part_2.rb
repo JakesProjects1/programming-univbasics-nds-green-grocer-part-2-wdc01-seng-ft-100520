64d1a68e440a49abb7450dd75336ca78aea326b5
Jacob Kagon <jacob@kagon.net>
2020-09-30 21:12:29 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,7 +4,31 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-end
+  coupCart = cart
+  count = 0
+  while coupons[count] do
+    if cart[coupons[count][:item]]
+      if cart[coupons[count][:item]][:count] >= coupons[count][:num]
+        coupCart[coupons[count][:item]][:count] = coupCart[coupons[count][:item]][:count] - coupons[count][:num]
+        coupItem = "#{coupons[count][:item]} W/COUPON"
+        coupPrice = coupons[count][:cost] / coupons[count][:num]
+        if coupCart[coupItem]
+          coupCount = coupCart[coupItem][:count] + coupons[count][:num]
+        else
+          coupCount = coupons[count][:num]
+        end
+        coupCart[coupItem] = {:price => coupPrice, :clearance => coupCart[coupons[count][:item]][:clearance], :count => coupCount}
+        count += 1
+      else
+        count += 1
+      end
+    else
+      count += 1
+    end
+  end
+  coupCart
+end	
+    
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
